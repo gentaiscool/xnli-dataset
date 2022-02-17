ece88f7b6f7e0913267ed024a4aa919451832ad2
gentaiscool <gentaindrawinata@gmail.com>
2022-02-17 00:20:21 -0500

    update files

--- preprocess.py
+++ preprocess.py
@@ -7,6 +7,7 @@ validation_data_per_lang = {}
 test_data_per_lang = {}
 
 lang_map = {}
+test_premise_hypothesis = {}
 
 for part in ["train", "validation", "test"]:
     print(part)
@@ -39,15 +40,24 @@ for part in ["train", "validation", "test"]:
                 if lang not in test_data_per_lang:
                     test_data_per_lang[lang] = []
                 test_data_per_lang[lang].append([i, premise, hypothesis, label])
+                test_premise_hypothesis[premise + " " + hypothesis] = True
 
 for lang in lang_map:
     print(f"train {lang} :", len(train_data_per_lang[lang]))
     print(f"validation {lang} :", len(validation_data_per_lang[lang]))
     print(f"test {lang} :", len(test_data_per_lang[lang]))
+    
     with open(f"xnli_train_{lang}.tsv", "w+") as f:
         f.write("id\tpremise\thypothesis\tlabel\n")
         for obj in train_data_per_lang[lang]:
             f.write(str(obj[0]) + "\t" + obj[1] + "\t" + obj[2] + "\t" + str(obj[3]) + "\n")
+    with open(f"xnli_train_filtered_{lang}.tsv", "w+") as f:
+        f.write("id\tpremise\thypothesis\tlabel\n")
+        for obj in train_data_per_lang[lang]:
+            if obj[1] + " " + obj[2] not in test_premise_hypothesis:
+                f.write(str(obj[0]) + "\t" + obj[1] + "\t" + obj[2] + "\t" + str(obj[3]) + "\n")
+            else:
+                print(f"{lang} filtered")
     with open(f"xnli_validation_{lang}.tsv", "w+") as f:
         f.write("id\tpremise\thypothesis\tlabel\n")
         for obj in validation_data_per_lang[lang]:
@@ -57,9 +67,11 @@ for lang in lang_map:
         for obj in test_data_per_lang[lang]:
             f.write(str(obj[0]) + "\t" + obj[1] + "\t" + obj[2] + "\t" + str(obj[3]) + "\n")   
 
+    os.system(f"zip -r xnli_train_filtered_{lang}.zip xnli_train_filtered_{lang}.tsv")
     os.system(f"zip -r xnli_train_{lang}.zip xnli_train_{lang}.tsv")
     os.system(f"zip -r xnli_validation_{lang}.zip xnli_validation_{lang}.tsv")
     os.system(f"zip -r xnli_test_{lang}.zip xnli_test_{lang}.tsv")
+    os.system(f"rm xnli_train_filtered_{lang}.tsv")
     os.system(f"rm xnli_train_{lang}.tsv")
     os.system(f"rm xnli_validation_{lang}.tsv")
     os.system(f"rm xnli_test_{lang}.tsv")
